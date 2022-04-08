30ee0c568ba89bca2274359d80ec2d6912b0068f
Rio Chandra <riochandra4@gmail.com>
2022-04-08 13:00:02 +0700

    fix parse args

--- jaksel-interpreter.js
+++ jaksel-interpreter.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 const fs = require('fs');
 
-const inputFile = () => {
+let fileLocate = null
+const parseArgs = () => {
   const args = process.argv;
-  if (args.length < 3) throw "Require file args, ex: 'node jaksel-interpreter.js jaksel'";
-  const exist = fs.existsSync(args[2]);
-  if (!exist) throw `File "${args[2]}" not found, please verify file location`;
-  return args[2];
-};
+  if (args.length < 3) {
+    console.log("Require file args, ex: 'node jaksel-interpreter.js example/example1.jaksel' or 'jaksel example/example1.jaksel'");
+    return false;
+  }
 
-const inputJaksel = fs.readFileSync(inputFile(), 'utf-8')
+  fileLocate = args[2];
+  if(!fs.existsSync(fileLocate)){
+    console.log(`File "${args[2]}" not found, please verify file location`)
+    return false;
+  }
+  return true;
+}
+
+if(!parseArgs()){
+  process.exit(1)
+}
+
+const inputJaksel = fs.readFileSync(fileLocate, 'utf-8')
 
 function flexing(input){
   let cmds = []
@@ -202,7 +214,7 @@ const execCmd = (cmds) => {
   if(isOpenGroup){
     resultCmds += ' }'
   }
-  // console.log(resultCmds)
+  
   eval(resultCmds)
 }
 
