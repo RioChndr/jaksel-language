729efe0e9f9e13c2e548676a0588a11c5da24a4f
devnazir <nazirbachrululum@gmail.com>
2022-04-17 19:44:35 +0700

    add responsive

--- web/src/pages/home/index.js
+++ web/src/pages/home/index.js
@@ -2,8 +2,8 @@ function Home() {
   return (
     <>
       <header className="mb-20">
-        <div className="mt-20">
-          <h1 className="text-6xl max-w-screen-lg m-auto text-center font-bold text-[#0F172A] tracking-tighter ">
+        <div className="mt-20 text-center">
+          <h1 className="md:text-6xl text-4xl max-w-screen-lg m-auto text-center font-bold text-[#0F172A] tracking-tighter ">
             Literally jaksel language itu sangat modern, easy to learn.
           </h1>
           <p className="text-center max-w-screen-md mx-auto mt-4">
@@ -11,18 +11,57 @@ function Home() {
             modern, easy to learn, using Indonesia-slang language. No
             programming experience required.
           </p>
+
+          <a
+            className="bg-yellow-300 py-3 px-6 mt-4 rounded-md font-medium mx-auto block w-max"
+            href="https://play-jaksel-language.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Playground
+          </a>
         </div>
 
         <iframe
           src="https://stackblitz.com/edit/node-p8pgzv?embed=1&amp;file=index.jaksel"
           frameBorder="0"
           title="hello-world"
-          className="aspect-video w-full mt-8 rounded-xl"
+          className="md:aspect-video aspect-square w-full mt-8 rounded-xl"
         ></iframe>
       </header>
 
+      <section className="section">
+        <h3 className="md:text-5xl text-3xl font-semibold text-center tracking-tighter">
+          Simplicity
+        </h3>
+        <div className="mt-5 grid md:grid-cols-2 gap-3">
+          <article className="flex flex-col font-medium bg-blue-400 text-white p-5">
+            <span className="block">PHP</span>
+            <code>&lt;?php echo "Hello World" ?&gt;</code>
+          </article>
+          <article className="flex flex-col font-medium bg-red-400 text-white p-5">
+            <span className="block">Java</span>
+            <code>
+              {`class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}`}
+            </code>
+          </article>
+          <article className="flex flex-col font-medium bg-yellow-400 text-black p-5">
+            <span className="block">JavaScript</span>
+            <code>console.log("Hello World")</code>
+          </article>
+          <article className="flex flex-col font-medium bg-yellow-300 text-black p-5">
+            <span className="block">Jaksel Script</span>
+            <code>spill "Hello World"</code>
+          </article>
+        </div>
+      </section>
+
       <section className="section text-center ">
-        <h2 className="text-5xl font-semibold text-center tracking-tighter">
+        <h2 className="md:text-5xl text-3xl font-semibold text-center tracking-tighter">
           Jaksel language is the future
         </h2>
         <p className="mt-5 max-w-screen-md mx-auto">
@@ -32,18 +71,6 @@ function Home() {
             "No programming experience required" {"  "}
           </span>
           thats it sih..
-        </p>
-      </section>
-
-      <section className="section"></section>
-
-      <section className="section ">
-        <h3 className="text-5xl font-semibold text-center tracking-tighter">
-          Why jaksel language is created ?
-        </h3>
-        <p className="mt-5 max-w-screen-md mx-auto text-center flex flex-col">
-          <span className="text-lg">&quot;Gabut&quot;</span>
-          <span className="text-sm">- Rio Chandra</span>
         </p>
       </section>
     </>
