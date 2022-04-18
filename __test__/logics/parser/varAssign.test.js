9c0171f185db966882858b8cbd6f12173f68ef51
Rio Chandra <riochandra4@gmail.com>
2022-04-18 22:36:13 +0700

    add unit test
    - condition if
    - consolelog
    - varassign
    - varReassign

--- __test__/logics/parser/varAssign.test.js
+++ __test__/logics/parser/varAssign.test.js
@@ -1,4 +1,5 @@
 const varAssign = require('../../../lib/logics/parser/varAssign')
+const getJsFormat = require('../test-parser-helper')
 
 describe('Test var assign', () => {
   it('Should parse string correctly', () => {
@@ -16,5 +17,15 @@ describe('Test var assign', () => {
     const test2 = varAssign("litetrayl foo i 123")
     expect(test1).toBe(null)
     expect(test2).toBe(null)
+  })
+
+  it('Should return correctly flexing', () => {
+    const jsFormat = getJsFormat(`
+      literally foo itu 123
+      literally bar itu "test"
+    `)
+    expect(jsFormat).not.toBeNull()
+    expect(jsFormat).toContain(`let foo = 123;`)
+    expect(jsFormat).toContain(`let bar = "test";`)
   })
 })
--- __test__/logics/parser/varReassign.test.js
+++ __test__/logics/parser/varReassign.test.js
@@ -1,6 +1,7 @@
 const varReassign = require('../../../lib/logics/parser/varReassign')
+const getJsFormat = require('../test-parser-helper')
 
-describe('Test var assign', () => {
+describe('Test var Reassign', () => {
   it('Should parse string correctly', () => {
     const test1 = varReassign("whichis foo itu 'foo'")
     expect(test1.exp).toBe(`foo = 'foo';`)
@@ -16,5 +17,15 @@ describe('Test var assign', () => {
     const test2 = varReassign("whi foo i 123")
     expect(test1).toBe(null)
     expect(test2).toBe(null)
+  })
+
+  it('Should return correctly flexing', () => {
+    const jsFormat = getJsFormat(`
+    literally foo itu "Hello world"
+    whichis foo itu "Changed"
+  `)
+    expect(jsFormat).not.toBeNull()
+    expect(jsFormat).toContain(`let foo = "Hello world";`)
+    expect(jsFormat).toContain(`foo = "Changed";`)
   })
 })
